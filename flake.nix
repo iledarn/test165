e1a3ff81b92bcddd7f9c3af0f18b52b256a706cc
Ildar Nasyrov <iledarnp@gmail.com>
2024-09-16 23:41:06 +0800

    fixing the name of repo

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     flake-utils.url = "github:numtide/flake-utils";
-    wkhtmltopdf-flake.url = "github:iledarn/WKHTMLTOPDFNIX";
+    wkhtmltopdf-flake.url = "github:iledarn/wkhtmltopodfnix";
   };
 
   outputs = { self, nixpkgs, flake-utils, wkhtmltopdf-flake }:
